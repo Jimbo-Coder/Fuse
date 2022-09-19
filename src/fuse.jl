865145eab712bed584ac56cfbc0a266f8de44fae
Jimbo-Coder <maxrizzo99@gmail.com>
2022-09-18 21:38:21 -0700

    Initializing

---
+++ src/fuse.jl
@@ -0,0 +1,25 @@
+module fuse
+using Plots, DataFrames, CSV, FITSIO
+
+include("test.jl")
+
+function data1()
+   b = FITSIO.read_header("Desktop\\Classes 4-1\\Research\\data\\VI_129_fuse.dat.fits")
+   c = FITSIO.FITS("Desktop\\Classes 4-1\\Research\\data\\VI_129_fuse.dat.fits")
+   return(b,c)
+end
+
+function counter(x)
+   df = DataFrame(x[2])
+   count = 0
+   for i in eachrow(df)
+       if i.Class == 81 && i.Aper == "LWRS"
+           count = count + 1
+       end
+   end
+   return(count)
+end
+
+
+
+end # module fuse
---
+++ src/test.jl
@@ -0,0 +1,6 @@
+
+function greett()
+   print("suck balls")
+end
+
+export(greett)
