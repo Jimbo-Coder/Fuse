bb6a5b20e44f764164c018688eb51055e0a82d08
Jimbo-Coder <maxrizzo99@gmail.com>
2022-10-24 20:19:25 -0700

    Test New Objects and development

--- src/fuse.jl
+++ src/fuse.jl
@@ -6,18 +6,26 @@ include("test.jl")
 function data1()
    b = FITSIO.read_header("Desktop\\Classes 4-1\\Research\\data\\VI_129_fuse.dat.fits")
    c = FITSIO.FITS("Desktop\\Classes 4-1\\Research\\data\\VI_129_fuse.dat.fits")
-   return(b,c)
+   return (b, c)
 end
 
 function counter(x)
    df = DataFrame(x[2])
    count = 0
    for i in eachrow(df)
-       if i.Class == 81 && i.Aper == "LWRS"
-           count = count + 1
-       end
+      if i.Class == 81 && i.Aper == "LWRS"
+         count = count + 1
+      end
    end
-   return(count)
+
+   count = 0
+   for i in eachrow(df)
+      if i.Class == 81
+         count = count + 1
+         print(i.Target, "  ", i.Aper, "  ", i.RAdeg, "    ", i.DEdeg, "\n")
+      end
+   end
+   return (count)
 end
 
 
