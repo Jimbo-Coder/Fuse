d21d02e9f6eec5f38fe206880e96c623ec612220
Jimbo-Coder <maxrizzo99@gmail.com>
2022-10-24 20:41:28 -0700

    Checking Test objects Julia

--- src/fuse.jl
+++ src/fuse.jl
@@ -19,12 +19,40 @@ function counter(x)
    end
 
    count = 0
+
+   objs = ["NGC7496",  
+   "NGC7714",
+   "NGC7673",
+   "NGC7662",
+   "NGC7496",
+   "NGC7469",
+   "NGC7354",
+   "NGC6826",
+   "NGC7094",
+   "NGC7090",
+   "NGC7078",
+   "NGC7027",
+   "NGC7023",
+   "NGC7009",
+   "NGC6992",
+   "NGC6905",
+   "NGC6891"]
+
+   
    for i in eachrow(df)
       if i.Class == 81
          count = count + 1
-         print(i.Target, "  ", i.Aper, "  ", i.RAdeg, "    ", i.DEdeg, "\n")
+         #print(i.Target, "  ", i.Aper, "  ", i.RAdeg, "    ", i.DEdeg, "\n")
+      end
+      
+      for j in range(1,16)
+         if i.Target == objs[j]
+            print(i)
+         end
       end
    end
+   
+
    return (count)
 end
 
